23c9d4df25eb86a36335a477beee9d4a28633b06
Fabian Jimenez <jose2181685@correo.uis.edu.co>
2023-03-22 19:55:10 -0500

    Instalacion Express
    Instalación y configuración de Express con un servidor básico como prueba

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Lab Clinico")
+const express = require("express");
+const app = express();
+const port = 3777;
+
+app.get("/", (req, res) =>{
+  res.send("Hola mi server en Express");
+});
+
+app.listen(port, () =>{
+  console.log("My port: " + port);
+});
