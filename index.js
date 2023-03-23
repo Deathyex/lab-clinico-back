f724e510fcde343089b10b4599370648d4f95328
Fabian Jimenez <jose2181685@correo.uis.edu.co>
2023-03-22 20:27:24 -0500

    Prueba peticiones

--- index.js
+++ index.js
@@ -6,6 +6,38 @@ app.get("/", (req, res) =>{
   res.send("Hola mi server en Express");
 });
 
+app.get("/inicio", (req, res) =>{
+  res.send("Inicio de Lab Clinico");
+});
+
+app.get("/examenes", (req, res) =>{
+  res.json([{
+    name: "Glucosa en orina",
+    price: 1000
+  },
+  {
+    name: "Identificacion de bacterias",
+    price: 1500
+  }]);
+});
+
+app.get('/examenes/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: "Glucosa en orina",
+    price: 1000
+  });
+});
+
+app.get('/categories/:categoryId/examenes/:examenId', (req, res) => {
+  const { categoryId, examenId } = req.params;
+  res.json({
+      categoryId,
+      examenId
+  });
+});
+
 app.listen(port, () =>{
   console.log("My port: " + port);
 });
