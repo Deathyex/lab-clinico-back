cc356ddaf82296fd33a99b023e29814921f24aef
Fabian Jimenez <jose2181685@correo.uis.edu.co>
2023-03-28 19:32:57 -0500

    docker y postgress

---
+++ libs/postgres.js
@@ -0,0 +1,17 @@
+const { Client } = require('pg');
+
+async function getConnection() {
+  const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    user: 'fabi',
+    password: 'admin123',
+    database: 'my_lab',
+
+  });
+  await client.connect();
+  return client;
+}
+
+module.exports = getConnection;
+
