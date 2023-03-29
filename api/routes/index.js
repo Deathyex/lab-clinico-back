27a5f17e3fe0d8af0e00229e4ba2092506bc1ad0
Fabian Jimenez <jose2181685@correo.uis.edu.co>
2023-03-28 19:45:51 -0500

    Conexion Node - Postgress

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 
 const examenesRouter = require('./examenes.router');
+const usersRouter = require('./users.router');
 
 function routerApi(app){
   const router = express.Router();
   app.use('/api/v1', router);
   router.use('/examenes', examenesRouter);
+  router.use('/users', usersRouter);
 }
 
 module.exports = routerApi;
