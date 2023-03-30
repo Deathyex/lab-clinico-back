f887291f165829a434b7080b3adc3cb4330ef24c
Fabian Jimenez <jose2181685@correo.uis.edu.co>
2023-03-29 19:09:57 -0500

    Sequelize ORM
    Instalacion y configuracion de sequelize en nuestro proyecto

--- api/services/examenes.service.js
+++ api/services/examenes.service.js
@@ -2,15 +2,13 @@ const { faker } = require("@faker-js/faker");
 const randomName = faker.name.fullName();
 const boom = require('@hapi/boom');
 
-const pool = require('../libs/postgres.pool');
+const sequelize = require('../libs/sequelize');
 
 class ExamenesService{
 
   constructor(){
     this.examenes = [];
     this.generate();
-    this.pool = pool;
-    this.pool.on('error', (err) => console.error(err));
   }
 
   generate(){
@@ -37,8 +35,8 @@ class ExamenesService{
 
   async find(){
     const query = 'SELECT * FROM tasks';
-    const rta = await this.pool.query(query);
-    return rta.rows;
+    const [data] = await sequelize.query(query);
+    return data;
   }
 
   async findOne(id){
