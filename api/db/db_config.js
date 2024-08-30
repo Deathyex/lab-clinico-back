a49a5456d1586c5fe667e992df2e7b2644b7ba28
Deathyex <yezithfernando@gmail.com>
2024-08-30 14:30:43 -0500

    Se establecio conexion con la BD

--- api/db/db_config.js
+++ api/db/db_config.js
@@ -1,3 +1,4 @@
+const { Sequelize } = require('sequelize');
 const { config } =  require('../config/config');
 
 // Extraer el nombre de usuario y contraseña de la configuración
@@ -6,6 +7,20 @@ const PASSWORD = encodeURIComponent(config.dbPassword);
 
 // Construir la URI de la base de datos
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+console.log(URI);
+
+
+const sequelize = new Sequelize(URI)
+
+/* async function testConnection(){
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+testConnection() */
 
 // Exportar configuraciones para los entornos de desarrollo y producción
 module.exports = {
