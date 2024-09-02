33ebca3f13805818813c9ec42d3245b5c2b17a19
Santiago Ariza <santiagoari02@outlook.com>
2024-09-02 16:48:30 -0500

    Se crea la función para enviar correos

--- api/config/config.js
+++ api/config/config.js
@@ -13,7 +13,9 @@ const config = {
   aws_name: process.env.AWS_BUCKET_NAME,
   aws_region: process.env.AWS_BUCKET_REGION,
   aws_key: process.env.AWS_PUBLIC_KEY,
-  aws_secret: process.env.AWS_SECRET_KEY="YG+VX9j2j4Y6a9wiwvy+9t93MuSoaozKG/sGf246"
+  aws_secret: process.env.AWS_SECRET_KEY="YG+VX9j2j4Y6a9wiwvy+9t93MuSoaozKG/sGf246",
+  emailUser: process.env.EMAIL_USER,
+  emailPass: process.env.EMAIL_PASS
 }
 
 module.exports =  { config };
